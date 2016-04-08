c4371155620072647c817e6b1c5c9fbf4ab59b8b
noobu <nobuhiro.nishi@gmail.com>
2016-04-08 09:07:21 +0000

    8.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @microposts = @user.microposts.order(created_at: :desc)
   end
   
+  
   def new
     @user = User.new
   end
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'プロフィールを編集しました'
+      redirect_to @user , notice: 'プロフィールを編集しました'
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
@@ -26,7 +27,7 @@ class UsersController < ApplicationController
   end
   
   
-   def create
+  def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
@@ -36,20 +37,20 @@ class UsersController < ApplicationController
     end
   end
 
-private
+ private
   
   def set_user
     @user= User.find(params[:id]) # urlでセットされているユーザーをとってきている
   end
   
   def user_params
-    params.require(:user).permit(:name, :email, :prfile,:area,
+    params.require(:user).permit(:name, :email, :profile,:area,
                                  :password, :password_confirmation)
   end
   
-    def user_edit
+  def user_edit
     unless @user == current_user
-    redirect_to root_path 
+    redirect_to root_path
     end
   end
   
